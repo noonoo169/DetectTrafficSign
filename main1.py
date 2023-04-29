6f3014cb708e71bb9d15d6da1b804f1753456f09
noo <ta642379@gmail.com>
2023-04-29 08:55:50 +0700

    Init project

---
+++ main1.py
@@ -0,0 +1,144 @@
+
+import os
+import platform
+import sys
+from pathlib import Path
+
+import torch
+
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[0]  # YOLOv5 root directory
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
+
+from models.common import DetectMultiBackend
+from utils.dataloaders import LoadStreams
+from utils.general import ( Profile,check_img_size, check_imshow, cv2, non_max_suppression,  scale_boxes)
+from utils.plots import Annotator, colors
+from utils.torch_utils import select_device, smart_inference_mode
+
+# import serial
+# import pynmea2
+
+# def getCurrentGPS():
+#     while True:
+#         port="/dev/ttyAMA0"
+#         ser=serial.Serial(port, baudrate=9600, timeout=0.5)
+#         dataout = pynmea2.NMEAStreamReader()
+#         newdata=ser.readline()
+
+#         if newdata[0:6] == "$GPRMC":
+#             newmsg=pynmea2.parse(newdata)
+#             lat=newmsg.latitude
+#             lng=newmsg.longitude
+#             gps = str(lat) + "," + str(lng)
+#             return gps
+
+
+from send import firebaseConfig
+import pyrebase
+import random
+
+firebase=pyrebase.initialize_app(firebaseConfig)
+db=firebase.database()
+
+def sendGPS(label):
+    data = {"LAT": random.randint(1,10), "LNG": random.randint(11,20), "Label": label}
+    db.update(data)
+    print(data)
+
+@smart_inference_mode()
+def run(
+        weights=ROOT / 'best.pt',  # model path or triton URL
+        data=ROOT / 'data/via-trafficsign.yaml',  # dataset.yaml path
+        imgsz=(128, 128),  # inference size (height, width)
+        conf_thres=0.7,  # confidence threshold
+        iou_thres=0.45,  # NMS IOU threshold
+        max_det=1000,  # maximum detections per image
+        device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+        view_img=False,  # show results
+        classes=None,  # filter by class: --class 0, or --class 0 2 3
+        agnostic_nms=False,  # class-agnostic NMS
+        augment=False,  # augmented inference
+        visualize=False,  # visualize features
+        line_thickness=3,  # bounding box thickness (pixels)
+        hide_labels=False,  # hide labels
+        hide_conf=False,  # hide confidences
+        half=False,  # use FP16 half-precision inference
+        dnn=False,  # use OpenCV DNN for ONNX inference
+        vid_stride=1,  # video frame-rate stride
+):
+    source = "0"
+    # Load model
+    device = select_device(device)
+    model = DetectMultiBackend(weights, device=device, dnn=dnn, data=data, fp16=half)
+    stride, names, pt = model.stride, model.names, model.pt
+    imgsz = check_img_size(imgsz, s=stride)  # check image size
+
+    # Dataloader
+    bs = 1  # batch_size
+    view_img = check_imshow(warn=True)
+    dataset = LoadStreams(source, img_size=imgsz, stride=stride, auto=pt, vid_stride=vid_stride)
+    bs = len(dataset)
+
+    # Run inference
+    model.warmup(imgsz=(1 if pt or model.triton else bs, 3, *imgsz))  # warmup
+    seen, windows, dt = 0, [], (Profile(), Profile(), Profile())
+    for path, im, im0s, _, _ in dataset:
+        with dt[0]:
+            im = torch.from_numpy(im).to(model.device)
+            im = im.half() if model.fp16 else im.float()  # uint8 to fp16/32
+            im /= 255  # 0 - 255 to 0.0 - 1.0
+            if len(im.shape) == 3:
+                im = im[None]  # expand for batch dim
+
+        # Inference
+        with dt[1]:
+           pred = model(im, augment=augment, visualize=visualize)
+
+        # NMS
+        with dt[2]:
+            pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
+        
+        temp = ""
+        index = -1
+        # Process predictions
+        for i, det in enumerate(pred):  # per image
+            seen += 1
+            # batch_size >= 1
+            p, im0 = path[i], im0s[i].copy()
+            
+
+            p = Path(p)  # to Path
+            annotator = Annotator(im0, line_width=line_thickness, example=str(names))
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_boxes(im.shape[2:], det[:, :4], im0.shape).round()
+
+                # Print results
+                index = int(det[0, 5].unique())
+                temp += f"{names[index]}"
+                # Write results
+                for *xyxy, conf, cls in reversed(det):
+                    if view_img:  # Add bbox to image
+                        c = int(cls)  # integer class
+                        label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')
+                        annotator.box_label(xyxy, label, color=colors(c, True))
+
+            # Stream results
+            im0 = annotator.result()
+        
+            if platform.system() == 'Linux' and p not in windows:
+                windows.append(p)
+                cv2.namedWindow(str(p), cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)  # allow window resize (Linux)
+                cv2.resizeWindow(str(p), im0.shape[1], im0.shape[0])
+            cv2.imshow(str(p), im0)
+            cv2.waitKey(1)
+       
+        if temp != "":
+            # gps = getCurrentGPS()
+            print("Vị trí nhãn:", index, temp)
+            sendGPS(temp)
+    
+run()
